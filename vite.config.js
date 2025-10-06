8cc7cd4d66790d874b157ce13ed142227c9ac68e
ParthaDey5 <parthadey.hm@gmail.com>
2025-10-06 13:02:00 +0530

    Updates ❤️

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/rock-paper-scissors-master/",
+  
   server: {
     port: 5175,
   },
